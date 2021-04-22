789c740348232c72523d98a5ca3a9f85b1f9a963
huntinghawk1415 <jacobwyattlockett@gmail.com>
2021-04-22 11:57:18 -0400

    i cant believe i left that in there im dead

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ const NotFound = props => {
   return (
     <div id="not-found" className="page c-flex align-center">
       <h1>404</h1>
-      <h2>Sucks to be you</h2>
+      <h2>Please contact us at 240-380-1505</h2>
     </div>
   )
 }
