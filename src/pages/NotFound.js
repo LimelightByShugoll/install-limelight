7bd8fd6a441e7d48e2832eb012ebc923ce32504a
huntinghawk1415 <jacobwyattlockett@gmail.com>
2021-06-04 14:17:53 -0400

    fixed some minor styles/updated fonts

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,13 @@ const NotFound = props => {
   return (
     <div id="not-found" className="page c-flex align-center">
       <h1>404</h1>
-      <h2>Please contact us at 240-380-1505</h2>
+      <h2>Please contact us at:</h2>
+      <h3 className="mt30">240-380-1505</h3>
+      <h3>
+        <a href="mailto:techsupport@limelightbyshugoll.com">
+          techsupport@limelightbyshugoll.com
+        </a>
+      </h3>
     </div>
   )
 }
